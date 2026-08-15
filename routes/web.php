3fa1c15a5d6a5a8adca40e1520086e7a560e321d
natshxml-bit <natshxml-bit@users.noreply.github.com>
2026-08-15 15:20:14 +0700

    cleanup: remove seed endpoint

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return response('OK', 200)->header('X-Healthcheck', 'true');
 });
-
-Route::get('/seed/{secret}', function ($secret) {
-    if ($secret !== 'absensi79seed') {
-        abort(403);
-    }
-    \Illuminate\Support\Facades\Artisan::call('import:students');
-    return response(\Illuminate\Support\Facades\Artisan::output());
-});
