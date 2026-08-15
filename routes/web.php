a7bc117afee496127abfffb7507c2f562792a0ac
natshxml-bit <natshxml-bit@users.noreply.github.com>
2026-08-15 15:07:51 +0700

    cleanup: remove temporary seed endpoint and OAuth callback route

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return response('OK', 200)->header('X-Healthcheck', 'true');
 });
-
-Route::get('/callback', function (Illuminate\Http\Request $request) {
-    return response()->json($request->only('code', 'error'));
-});
-
-Route::get('/seed/{secret}', function ($secret) {
-    if ($secret !== 'absensi79seed') {
-        abort(403);
-    }
-    \Illuminate\Support\Facades\Artisan::call('db:seed', ['--force' => true, '--class' => 'FullDatabaseSeeder']);
-    return response(\Illuminate\Support\Facades\Artisan::output());
-});
